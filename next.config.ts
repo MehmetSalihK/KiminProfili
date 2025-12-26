ef9138d613943eb5126ce64720509f636f2b46e2
MehmetSalihK <salihketur60@gmail.com>
2025-12-26 08:12:56 +0100

    Fix Vercel 502: Enforce HTTPS and update next.config image domains

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
+      { protocol: 'https', hostname: 'randomuser.me' },
+      { protocol: 'https', hostname: 'ws-public.interpol.int' },
+      { protocol: 'https', hostname: '*.licdn.com' },
+      { protocol: 'https', hostname: '*.googleusercontent.com' },
+      { protocol: 'https', hostname: '**' },
     ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   /* config options here */
 };
